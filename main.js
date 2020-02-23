02c9527baca86448afad45e9d1adc970bba33ba3
pallab-hash <pallab.acharyya@centurylink.com>
2020-02-23 14:15:12 +0000

    Lab 2

--- main.js
+++ main.js
@@ -155,12 +155,7 @@ function post(serviceNowTable, callback) {
 
 
 
-//*
- * This section is used to test your project.
- * We will test both get() and post() functions.
- * If either function returns data, print the returned data to console on STDOUT.
- * If either function returns an error, print the returned data to the console on STDERR.
- */
+
 function main() {
   get('change_request', (data, error) => {
     if (error) {
